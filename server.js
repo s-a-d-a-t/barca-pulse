5fd7efa2e4959e39c9b72b5a74152d34cd3a8ca8
Sadat Amir <sdrkk66@gmail.com>
2026-03-11 21:27:52 +0300

    fixed server issue

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 
-// server.js
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -11,12 +11,17 @@ app.use(cors());
 const PORT = process.env.PORT || 3000;
 const NEWS_API_KEY = process.env.NEWS_API_KEY;
 
+// serve frontend
+app.use(express.static(path.join(__dirname, 'client')));
+
 app.get('/news', async (req, res) => {
   res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+
   try {
     const response = await axios.get(
       `https://newsapi.org/v2/everything?q="FC Barcelona" OR "Barça"&sortBy=publishedAt&language=en&pageSize=50&apiKey=${NEWS_API_KEY}&t=${Date.now()}`
     );
+
     res.json(response.data);
   } catch (err) {
     res.status(500).json({ message: 'Error fetching news', error: err.message });
@@ -24,5 +29,5 @@ app.get('/news', async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
